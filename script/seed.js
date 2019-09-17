be2d3dbcf5c5188626dde58ebd4d93fa8551828f
GreysonFlippo1 <greysonflippo@gmail.com>
2019-09-17 16:39:05 -0500

    added more seeded data for user and animals. Prepared but NOT functional seeding for animal categories

--- script/seed.js
+++ script/seed.js
@@ -8,8 +8,15 @@ async function seed() {
   console.log('db synced!')
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+    User.create({email: 'cody@email.com', password: '123', admin: true}),
+    User.create({email: 'murphy@email.com', password: '123', admin: false}),
+    User.create({email: 'admin@admin.com', password: 'admin', admin: true}),
+    User.create({email: 'guest@email.com', password: '123', admin: false}),
+    User.create({
+      email: 'boyardee@email.com',
+      password: 'ravioli',
+      admin: false
+    })
   ])
   console.log(`seeded ${users.length} users`)
 
@@ -26,8 +33,51 @@ async function seed() {
       cost: 64.0,
       description: '0-2 porkchops worth.',
       photo: 'http://www.minecraftinformation.com/images/pig.png'
+    }),
+    Animal.create({
+      name: 'Kiwi Bird',
+      cost: 4.99,
+      photo:
+        'https://www.doc.govt.nz/thumbs/hero/contentassets/a450e32f0b824531858d566404c21884/southern-brown-kiwi-tokoeka-stewart-island-photo-credit-alina-thiebes1920.jpg'
+    }),
+    Animal.create({
+      name: 'Baboon',
+      cost: 2450.99,
+      photo:
+        'https://kids.sandiegozoo.org/sites/default/files/2018-07/animal-hero-hamadryas_0.jpg'
     })
   ])
+  console.log(`seeded ${animals.length} animals`)
+
+  /*
+  THESE ARE OBJECTS TO SEED FOR ANIMAL CATEGORIES AND CATEGORIES WHEN THEY ARE COMPLETE
+  ** import the models when ready
+
+  const categories = await Promise.all([
+    Category.create({category: "Farm Animals"});
+    Category.create({category: "Large"});
+    Category.create({category: "Intelligent"});
+    Category.create({category: "exotic"});
+    Category.create({category: "Bird"});
+    }),
+  ])
+  
+  console.log(`seeded ${categories.length} categories`)
+
+  const animalCategories = await Promise.all([
+    AnimalCategory.create({categoryId:1,animalId:1});
+    AnimalCategory.create({categoryId:2,animalId:2});
+    AnimalCategory.create({categoryId:3,animalId:1});
+    AnimalCategory.create({categoryId:3,animalId:2});
+    AnimalCategory.create({categoryId:4,animalId:2});
+    AnimalCategory.create({categoryId:5,animalId:3});
+    AnimalCategory.create({categoryId:3,animalId:4});
+    AnimalCategory.create({categoryId:4,animalId:4});
+  ])
+
+  console.log(`seeded ${animalCategories.length} animal categories`)
+
+  */
 
   console.log(`seeded successfully`)
 }
