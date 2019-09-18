8a15de16b29f60b38ed02e07f174a887a9926288
M. Wislek <mike.wislek@gmail.com>
2019-09-18 13:56:03 -0500

    Update navigation * Intall bootstrap .css components * Update webpack to compile css * Update navigation to use bootstrap Navbar component

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -2,30 +2,36 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link, NavLink} from 'react-router-dom'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+
 import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          <h1>beasts.co LOGO</h1>
-          <Link to="/home">HOME</Link>
-          <NavLink to="/animals">ANIMALS</NavLink>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
+const Navigation = ({handleClick, isLoggedIn}) => (
+  <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="/home">beast.co LOGO</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="/home">HOME</Nav.Link>
+      <Nav.Link href="/animals">ANIMALS</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form>
+    {isLoggedIn ? (
+      <a href="#" onClick={handleClick}>
+        Logout
+      </a>
+    ) : (
+      <div>
+        <Nav.Link href="/login">Login</Nav.Link>
+        <Nav.Link href="/signup">Sign Up</Nav.Link>
+      </div>
+    )}
+  </Navbar>
 )
 
 /**
@@ -45,12 +51,12 @@ const mapDispatch = dispatch => {
   }
 }
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(Navigation)
 
 /**
  * PROP TYPES
  */
-Navbar.propTypes = {
+Navigation.propTypes = {
   handleClick: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
 }
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
         loader: 'babel-loader'
       },
       {
-        test: /`.css$/,
+        test: /.css$/,
         use: ['style-loader', 'css-loader']
       }
     ]
