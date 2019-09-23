991e75ca0b7982d30c53902f4a41ea3276e7dd96
M. Wislek <mike.wislek@gmail.com>
2019-09-23 09:27:21 -0500

    Completed checkout functionality
    * Updated database to simplify order structure for client
    * Updated nav to restructure and simplify
    * Added icon package

--- server/db/models/animalOrder.js
+++ server/db/models/animalOrder.js
@@ -2,11 +2,6 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const AnimalOrder = db.define('animalOrder', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
   quantity: {
     type: Sequelize.INTEGER,
     allowNull: false,
