96a7f198b231c1fb4961a9cd37812718f93a4224
GreysonFlippo1 <greysonflippo@gmail.com>
2019-09-24 13:06:43 -0500

    trimmed off imports that were not used in admins routes

--- client/components/Admin/admin.js
+++ client/components/Admin/admin.js
@@ -1,8 +1,5 @@
 import React, {Component} from 'react'
-import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import Container from 'react-bootstrap/Container'
-import Form from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Button from 'react-bootstrap/Button'
